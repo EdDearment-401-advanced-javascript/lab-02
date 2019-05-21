cd600fafc9ca3f1d80b7e1b9076b03fdd466a8aa
ekearm <ed787.ed@gmail.com>
2019-05-21 14:55:21 -0700

    done wit da objects

--- objects/vehicle-factory.js
+++ objects/vehicle-factory.js
@@ -1,7 +1,33 @@
-
 'use strict';
 
+const Vehicle = () => ({
+  drive: () => {
+    return 'Moving Forward';
+  },
+  stop: () => {
+    return 'Stopping';
+  }
+});
+
 function CarFactory() {
+  let car = Object.assign(
+    {wheels: 4},
+    {name},
+    Vehicle()
+    );
+
+  return Object.freeze(car);
 }
 
-module.exports = CarFactory;
+function MotorcycleFactory() {
+    let moto = Object.assign(
+        {wheels: 2},
+        {name},
+        {wheelie},
+        Vehicle()
+    );
+    function wheelie() {return 'Wheee!'}
+    return Object.freeze(moto);
+}
+
+module.exports = {CarFactory, MotorcycleFactory};
